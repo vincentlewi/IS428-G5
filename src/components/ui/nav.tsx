d8cc42cbaeb32ae7729897c31d7fcc69fed7aa1f
emilyaurelia <emilyaurelia01@gmail.com>
2024-03-28 22:31:47 +0800

    feat: overview page template done

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -8,14 +8,14 @@ export default function Nav(props: NavProps) {
   const activePage = props.activePage
 
   return (
-    <nav className="flex items-center justify-between py-4">
+    <nav className="flex items-center justify-between py-4 px-4 sticky top-0 z-40 bg-white">
       <div>
         <a href="/" className="text-gray-800">Logo</a>
       </div>
       <div className="flex h-5 items-center space-x-4">
         <a href="/" className={`text-gray-800 ${activePage === 'overview' ? 'font-semibold' : ''}`}>Overview</a>
         <Separator orientation="vertical" />
-        <a href="/recommend" className={`text-gray-800 ${activePage === 'recommend' ? 'font-semibold' : ''}`}>Find your Ideal Home</a>
+        <a href="/recommend" className={`text-gray-800 ${activePage === 'recommend' ? 'font-semibold' : ''}`}>Dashboard</a>
       </div>
       <div></div>
     </nav>
